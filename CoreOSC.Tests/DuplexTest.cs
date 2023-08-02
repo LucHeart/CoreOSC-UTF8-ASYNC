7b953fd789f8e0851d746cedb4eeb63a480e2678
LucHeart <luca@perugia.ovh>
2023-08-02 02:33:12 +0200

    Add duplex tests

---
+++ CoreOSC.Tests/DuplexTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace LucHeart.CoreOSC.Tests;
+
+public class DuplexTest
+{
+    /// <summary>
+    /// Duplex connection simple message test
+    /// </summary>
+    [Fact(Timeout = 1000)]
+    public async Task DuplexSendReceive()
+    {
+        var endpoint = TestUtils.GetNextEndpoint();
+        using var duplex = new UdpDuplex(endpoint, endpoint);
+        
+        var msg = new OscMessage("/test/", 23.42f);
+        await duplex.SendAsync(msg);
+        var received = (await duplex.ReceiveAsync()).AsT0;
+        Assert.Equal("/test/", received.Address);
+        Assert.Equal(23.42f, received.Arguments[0]);
+    }
+    
+    /// <summary>
+    /// Duplex connection message with utf8 content
+    /// </summary>
+    [Fact(Timeout = 1000)]
+    public async Task DuplexUft8()
+    {
+        var endpoint = TestUtils.GetNextEndpoint();
+        using var duplex = new UdpDuplex(endpoint, endpoint);
+    
+        var msg = new OscMessage("/test/", "⚡");
+        await duplex.SendAsync(msg);
+        var received = (await duplex.ReceiveAsync()).AsT0;
+        Assert.Equal("/test/", received.Address);
+        Assert.Equal("⚡", received.Arguments[0]);
+    }
+}
