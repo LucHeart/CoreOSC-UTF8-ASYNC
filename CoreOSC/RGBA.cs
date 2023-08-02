6f5ba2a65da8d64035c08e8b440e8bf3103017c0
LucHeart <luca@perugia.ovh>
2023-08-02 02:39:59 +0200

    Remove stupid code from RGBA... please dont do if(cond) return true else return false.... i cry

--- CoreOSC/RGBA.cs
+++ CoreOSC/RGBA.cs
@@ -1,58 +1,29 @@
 ﻿namespace LucHeart.CoreOSC;
 
-public struct RGBA
+// ReSharper disable once InconsistentNaming
+public readonly struct RGBA
 {
-    public byte R;
-    public byte G;
-    public byte B;
-    public byte A;
+    public readonly byte R;
+    public readonly byte G;
+    public readonly byte B;
+    public readonly byte A;
 
     public RGBA(byte red, byte green, byte blue, byte alpha)
     {
-        this.R = red;
-        this.G = green;
-        this.B = blue;
-        this.A = alpha;
+        R = red;
+        G = green;
+        B = blue;
+        A = alpha;
     }
 
-    public override bool Equals(System.Object obj)
+    public override bool Equals(object obj) => obj switch
     {
-        if (obj.GetType() == typeof(RGBA))
-        {
-            if (this.R == ((RGBA)obj).R && this.G == ((RGBA)obj).G && this.B == ((RGBA)obj).B && this.A == ((RGBA)obj).A)
-                return true;
-            else
-                return false;
-        }
-        else if (obj.GetType() == typeof(byte[]))
-        {
-            if (this.R == ((byte[])obj)[0] && this.G == ((byte[])obj)[1] && this.B == ((byte[])obj)[2] && this.A == ((byte[])obj)[3])
-                return true;
-            else
-                return false;
-        }
-        else
-            return false;
-    }
+        RGBA rgba => R == rgba.R && G == rgba.G && B == rgba.B && A == rgba.A,
+        byte[] byteObj => R == byteObj[0] && G == byteObj[1] && B == byteObj[2] && A == byteObj[3],
+        _ => false
+    };
 
-    public static bool operator ==(RGBA a, RGBA b)
-    {
-        if (a.Equals(b))
-            return true;
-        else
-            return false;
-    }
-
-    public static bool operator !=(RGBA a, RGBA b)
-    {
-        if (!a.Equals(b))
-            return true;
-        else
-            return false;
-    }
-
-    public override int GetHashCode()
-    {
-        return (R << 24) + (G << 16) + (B << 8) + (A);
-    }
+    public static bool operator == (RGBA a, RGBA b) => a.Equals(b);
+    public static bool operator != (RGBA a, RGBA b) => !a.Equals(b);
+    public override int GetHashCode() => (R << 24) + (G << 16) + (B << 8) + A;
 }
